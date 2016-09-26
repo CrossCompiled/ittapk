0de94e903d8e1d751162d8782785ab8b21b8750d
KasperSK <raizzat@gmail.com>
2016-09-26 13:02:37 +0200

    1.4 done

--- lecture09/src/exercise02/radio.cpp
+++ lecture09/src/exercise02/radio.cpp
@@ -8,6 +8,9 @@
 #include <boost/statechart/state_machine.hpp>
 #include <boost/statechart/simple_state.hpp>
 #include <boost/statechart/transition.hpp>
+#include <boost/statechart/custom_reaction.hpp>
+#include <boost/statechart/deep_history.hpp>
+#include <boost/statechart/shallow_history.hpp>
 
 
 /**
@@ -40,35 +43,158 @@ void print(unsigned int lvl, const std::string& name)
 
 namespace sc = boost::statechart;
 
-
 /**
  * Events that can be handled
  */
 
-
+struct EvAMTuner : sc::event<EvAMTuner>{};
+struct EvFMTuner : sc::event<EvFMTuner>{};
+struct EvOn : sc::event<EvOn>{};
+struct EvOff : sc::event<EvOff>{};
+// new events"!
+struct EvTuner : sc::event<EvTuner> {};
+struct EvCD : sc::event<EvCD> {};
+struct EvCDInserted : sc::event<EvCDInserted> {};
+struct EvEject : sc::event<EvEject> {};
+struct EvCDState : sc::event<EvCDState>{
+    EvCDState(bool valid) : isValid(valid){};
+    bool GetState() const {
+        return isValid;
+    }
+    private:
+    bool isValid;
+};
 
 /**
  * Machine definition and the top level states
  * forward declarations
  */
+struct Off;
+struct On;
+struct MeanMachine : sc::state_machine<MeanMachine, Off>{
+    bool cdIn = false;
+    //empty struct
 
+};
 
+struct CDLoading;
+struct CDPlaying;
+struct RadioPlaying;
+
+/**
+ * On
+ */
+struct On : sc::simple_state<On, MeanMachine, RadioPlaying>{
+    PRINT_ENTRY_EXIT(0, On);
+    typedef sc::transition<EvOff, Off> reactions;
+};
+
+/**
+ * Off
+ */
+struct Off : sc::simple_state<Off, MeanMachine>{
+    PRINT_ENTRY_EXIT(0, Off);
+    typedef sc::transition<EvOn, On> reactions;
+};
+
+/**
+ * Radioplaying
+ */
+struct FMTuner;
+struct AMTuner;
+
+struct RadioPlaying : sc::simple_state<RadioPlaying, On, FMTuner>{
+    PRINT_ENTRY_EXIT(1, RadioPlaying);
+    typedef boost::mpl::list<sc::transition<EvCDInserted, CDLoading>, sc::custom_reaction<EvCD>> reactions;
+    sc::result react(const EvCD& kalleNavn){
+        if(context<MeanMachine>().cdIn){
+            return transit<CDPlaying>();
+        }
+        std::cout << "No cd insertet" << std::endl;
+        return discard_event();
+    }
+};
 
 /**
  * FMTuner
  */
 
+struct FMTuner : sc::simple_state<FMTuner, RadioPlaying>{
+    PRINT_ENTRY_EXIT(2, FMTuner);
+    typedef sc::transition<EvAMTuner, AMTuner> reactions;
+};
 
 
 /**
  * AMTuner
  */
 
+struct AMTuner : sc::simple_state<AMTuner, RadioPlaying>{
+    PRINT_ENTRY_EXIT(2, AMTuner);
+    typedef sc::transition<EvFMTuner, FMTuner> reactions;
+};
+
+/**
+ * CDLoading
+ */
+struct CDLoading : sc::simple_state<CDLoading, On>
+{
+    /* Missing code */
+    typedef sc::custom_reaction<EvCDState> reactions;
+
+    PRINT_ENTRY_EXIT(1, CDLoading);
+
+    /* Missing code */
+    sc::result react(const  EvCDState& kalleNavn){
+        if(kalleNavn.GetState())
+            return transit<CDPlaying>();
+        else
+            return transit<RadioPlaying>();
+    }
+};
+
+
+/**
+ * CDPlaying
+ */
+struct CDPlaying : sc::simple_state<CDPlaying, On>
+{
+    typedef sc::transition<EvTuner, RadioPlaying> reactions;
+
+    PRINT_ENTRY_EXIT(1, CDPlaying);
+};
+
 
 
 int main()
 {
     /* Instantiate State machine and make it process some relevant events... */
+
+    MeanMachine MyMeanMachine;
+    MyMeanMachine.initiate();
+    //MyMeanMachine.cdIn = false;
+
+    std::cout << "Sending EvOn" << std::endl;
+    MyMeanMachine.process_event(EvOn());
+    std::cout << "Sending EvAMTuner" << std::endl;
+    MyMeanMachine.process_event(EvAMTuner());
+    std::cout << "Sending EvCDInserted" << std::endl;
+    MyMeanMachine.process_event(EvCDInserted());
+    std::cout << "Sending EvCDState(true)" << std::endl;
+    MyMeanMachine.process_event(EvCDState(true));
+    std::cout << "Sending EvTuner" << std::endl;
+    MyMeanMachine.process_event(EvTuner());
+    std::cout << "Sending EvCD" << std::endl;
+    MyMeanMachine.process_event(EvCD());
+    MyMeanMachine.cdIn = true;
+    std::cout << "Sending EvCD" << std::endl;
+    MyMeanMachine.process_event(EvCD());
+    std::cout << "Sending EvOff" << std::endl;
+    MyMeanMachine.process_event(EvOff());
+    std::cout << "Sending EvOn" << std::endl;
+    MyMeanMachine.process_event(EvOn());
+    std::cout << "Sending EvTuner" << std::endl;
+    MyMeanMachine.process_event(EvTuner());
 
     return 0;
 }
